f7926d8a67836d38cdfbc4973937b7ff9d99fe00
xuyu <804712680@qq.com>
2021-08-18 22:48:53 +0800

    22

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -1,14 +1,12 @@
 package lambdasinaction.chap1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class FilteringApples{
 
     public static void main(String ... args){
-        System.out.println("master 的修改");
+
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                                               new Apple(155, "green"),
                                               new Apple(120, "red"));	
