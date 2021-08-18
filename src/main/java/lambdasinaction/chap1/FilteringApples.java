53d6214d6bc327352baa12513723d29690da1652
xuyu <804712680@qq.com>
2021-08-18 21:12:16 +0800

    1

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -1,12 +1,14 @@
 package lambdasinaction.chap1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class FilteringApples{
 
     public static void main(String ... args){
-
+        System.out.println("这是panda的修改");
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                                               new Apple(155, "green"),
                                               new Apple(120, "red"));	
@@ -38,6 +40,7 @@ public class FilteringApples{
         for (Apple apple: inventory){
             if ("green".equals(apple.getColor())) {
                 result.add(apple);
+
             }
         }
         return result;
